b85db4e46f7f54a86ffc2c26cd965893093a414f
jack <jrfisch95@gmail.com>
2023-07-09 12:36:08 -0400

    initial commit. set up new nestjs project. add mongoose cat schema and owner schema.

---
+++ src/cats/schemas/cat.schema.ts
@@ -0,0 +1,42 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Owner } from '../../owners/schemas/owner.schema';
+
+@Schema()
+export class Cat {
+  @Prop({
+    required: true,
+    minlength: 1,
+    maxlength: 64,
+  })
+  name: string;
+
+  @Prop({
+    required: true,
+    minlength: 1,
+    maxlength: 64,
+  })
+  age: string;
+
+  @Prop({
+    required: true,
+    minlength: 1,
+    maxlength: 64,
+  })
+  breed: string;
+
+  /**
+   * Schema types are automatically inferred through typescript metadata and reflection capabilities.
+   * However, when types cannot be implicitly reflected such as in arrays or nested object structures,
+   * types must be explicitly indicated as follows:
+   */
+  @Prop([String])
+  tags: string[];
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Owner', required: true })
+  owner: Owner;
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat);
+
+export type CatDocument = HydratedDocument<Cat>;
---
+++ src/owners/schemas/owner.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Cat } from '../../cats/schemas/cat.schema';
+
+@Schema()
+export class Owner {
+  @Prop({
+    required: true,
+    minlength: 1,
+    maxlength: 64,
+  })
+  firstName: string;
+
+  @Prop({
+    required: true,
+    minlength: 1,
+    maxlength: 64,
+  })
+  lastName: string;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Cat' }],
+  })
+  cats: Cat[];
+}
+
+export const OwnerSchema = SchemaFactory.createForClass(Owner);
+
+export type OwnerDocument = HydratedDocument<Owner>;
